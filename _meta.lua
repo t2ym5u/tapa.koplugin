28d60cce5c81f2d7bdc8bd4138140124fbfd7157
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-15 14:21:48 +0200

    chore: bump version to 1.1.1

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "tapa",
     fullname    = _("Tapa"),
     description = _("Shade cells to form a connected wall"),
-    version     = "1.1.0",
+    version     = "1.1.1",
 }
