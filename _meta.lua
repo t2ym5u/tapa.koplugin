1c6b2b0518f3eb5c3b4d0c0eff920952df25d6e8
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-31 17:48:02 +0200

    chore: bump version to 1.1.11

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     fullname    = _("Tapa"),
     description = _("Shade cells to form a connected wall"),
-    version     = "1.1.10",
+    version     = "1.1.11",
 }
