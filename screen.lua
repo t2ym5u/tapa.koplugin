87ada4a9631a6d45716f5fe780bfd2f0da40ae9a
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-04 12:43:06 +0200

    refactor: symlink common/ to shared game-common

--- screen.lua
+++ screen.lua
@@ -105,6 +105,8 @@ function TapaScreen:buildLayout()
         and math.max(right_panel_width - Size.span.horizontal_default, 100)
         or  math.floor(sw * 0.9)
 
+    self.status_text:setMaxWidth(is_landscape and button_width or board_frame_size)
+
     local title_bar = self:buildTitleBar(_("Tapa"), function()
         return {
             { text = _("New game"),            callback = function() self:onNewGame() end },
